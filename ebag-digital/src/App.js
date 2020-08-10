24ae2db8fa5f2bdb327edd6138e4cc752d6e4524
gabesharpton <gabriel.sharpton@gmail.com>
2020-08-10 13:05:46 -0500

    added react app and cleaned basic structure: Starting point

---
+++ ebag-digital/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/header'
+import Intro from './components/intro'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header />
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
